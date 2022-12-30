7c1e26c6e1457b3ad2c2cb0701fe7415d82f6d7d
Hamxay <hamzach1288@gmail.com>
2022-12-30 18:13:54 +0500

    enviroment

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\mohsin\Pictures\project\env
+set VIRTUAL_ENV=C:\Users\ss\Documents\Clinic-Managment-System\env
 
 if not defined PROMPT set PROMPT=$P$G
 
